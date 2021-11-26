db0f2d17e32df1f6a44772aeea664a4e9a073b71
HaiBinhFX11493 <binhphfx11493@funix.edu.vn>
2021-11-27 01:44:43 +0700

    sua lai coment

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,20 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import {STAFFS} from "./Components/StaffList/staffs.jsx";
 
-//control data
+//Control
 function AppData() {
-  var controlData=STAFFS.map(staff =><div>{staff.name}</div>)
-
-  function In() {
-    console.log("params") 
-  }
+  var controlData=STAFFS.map(staff => <div>{staff.name}</div>)
   return (
     <Hienthi1 name={controlData} />
   );
 }
 
-
-//khung
+//Body
 function Hienthi1(props) {
   return <div className="App">
     <div className="HeadingTop">
@@ -23,20 +18,20 @@ function Hienthi1(props) {
     </div>
     <div className="chiaBang">
       <div className="contentBody">
-      {/* {props.name.map( e => <Hienthi2 childField={e} />)} */}
-      <Hienthi2 childField={props.name[0]} />
-      <Hienthi2 childField={props.name[1]} />
-      <Hienthi2 childField={props.name[2]} />
-      <Hienthi2 childField={props.name[3]} />
-      <Hienthi2 childField={props.name[4]} />
-      <Hienthi2 childField={props.name[5]} />
+        {/* {props.name.map( e => <Hienthi2 childField={e} />)} */}
+        <Hienthi2 childField={props.name[0]} />
+        <Hienthi2 childField={props.name[1]} />
+        <Hienthi2 childField={props.name[2]} />
+        <Hienthi2 childField={props.name[3]} />
+        <Hienthi2 childField={props.name[4]} />
+        <Hienthi2 childField={props.name[5]} />
       </div>
       <p className="contentP">Bấm vào tên nhân viên để xem thông tin</p>
     </div>
   </div>
 }
 
-//noidung 1 button
+//noidung Body
 function Hienthi2(props) {
   return <div className="contentName" key={STAFFS.id}>{props.childField}</div>
 }
